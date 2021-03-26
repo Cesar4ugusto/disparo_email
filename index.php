1c82d3ad3228e5d075a1f945294841a662be5e8a
César Augusto <cesarapolidorio@gmail.com>
2021-03-26 10:23:37 -0300

    Melhorias de aparência

--- index.php
+++ index.php
@@ -1,44 +1,57 @@
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+    <title>Disparo de Email</title>
+</head>
+<body>
+    
+    <?php
 
-    //Import PHPMailer classes into the global namespace
-    //These must be at the top of your script, not inside a function
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+        //Import PHPMailer classes into the global namespace
+        //These must be at the top of your script, not inside a function
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
 
-    //Load Composer's autoloader
-    require 'vendor/autoload.php';
+        //Load Composer's autoloader
+        require 'vendor/autoload.php';
 
-    //Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+        //Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                                       //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
-        $mail->SMTPSecure = 'tls';                                  //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                       //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'useremail';                     //SMTP username
+            $mail->Password   = 'passwordemail';                               //SMTP password
+            $mail->SMTPSecure = 'tls';                                  //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+            //Recipients
+            $mail->setFrom('useremail', 'name');
+            $mail->addAddress('destinationemail', 'destinationname');     //Add a recipient
 
-        $template = file_get_contents('../template_email.html');
+            $template = file_get_contents('./template_email.html');
 
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = $template; /*'This is the HTML message body <b>in bold!</b>';*/
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = $template; /*'This is the HTML message body <b>in bold!</b>';*/
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    
+    ?>
 
-?>
+</body>
+</html>
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+![capagit](https://user-images.githubusercontent.com/67242974/112635878-1c994500-8e1b-11eb-94f0-c017b159c160.jpg)
+
 # Disparo de Email com Template.
 
 [![Github Badge](https://img.shields.io/badge/-Github-000?style=flat-square&logo=Github&logoColor=white&link=https://github.com/Cesar4ugusto)](https://github.com/Cesar4ugusto)
@@ -12,6 +14,8 @@ Sou estudante de Análise e Desenvolvimento de Sistemas focado no desenvolviment
 ## Este projeto
 
 > Status do Projeto: Concluido :heavy_check_mark:
+
+> Projeto Back-End :+1:
 
 Tem como propósito ajudar desenvolvedores no envio de email atravez da funcionalidade do PHP, o `PHPMailer`. Conta também com um template de email configurado para ser enviado como corpo do projeto.
 
@@ -40,3 +44,9 @@ $mail->Body = $template;
 
 - Suporte SMTP integrado - Envio de um servidor de email local.
 - Compatível com PHP 5.5 e posterior, incluindo PHP 8.0.
+
+### Licença
+
+As imagens estão sob proteção da marca.
+
+Todos os ativos e código são de domínio público, a menos que especificado de outra forma.
